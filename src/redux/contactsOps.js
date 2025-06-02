0b7214c953f1d1b09a307a14be4ee8309b846a5f
ihorborys <ihorborys@gmail.com>
2025-06-02 21:04:15 +0300

    Edited name getContactsThunk to fetchContacts

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -3,7 +3,17 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = "https://6839987e6561b8d882b0fcda.mockapi.io/";
 
-export const fetchContacts = createAsyncThunk("contacts/get-all", async () => {
+export const fetchContacts = createAsyncThunk("contacts/fetchAll", async () => {
   const { data } = await axios("Contacts");
   return data;
 });
+
+export const addContact = createAsyncThunk("contacts/addContact", async () => {
+  const { data } = await axios.post("Contact");
+  return data;
+});
+
+// export const deleteContact = createAsyncThunk("contacts/deleteContact, async () => {
+//   const { data } = await axios("Contacts");
+//   return data;
+// });
