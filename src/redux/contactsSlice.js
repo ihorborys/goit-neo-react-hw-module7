8cb185b308b056fd8f99df2895abf6f0e84203f8
ihorborys <ihorborys@gmail.com>
2025-06-02 23:21:08 +0300

    Edited addContact functionality

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -37,18 +37,18 @@ const contactsSlice = createSlice({
       .addCase(addContact.rejected, (state, { payload }) => {
         state.error = payload;
         state.loading = false;
-      })
-      .addCase(deleteContact.pending, (state) => {
-        state.loading = true;
-      })
-      .addCase(deleteContact.fulfilled, (state, { payload }) => {
-        state.items = payload;
-        state.loading = false;
-      })
-      .addCase(deleteContact.rejected, (state, { payload }) => {
-        state.error = payload;
-        state.loading = false;
       });
+    // .addCase(deleteContact.pending, (state) => {
+    //   state.loading = true;
+    // })
+    // .addCase(deleteContact.fulfilled, (state, { payload }) => {
+    //   state.items = payload;
+    //   state.loading = false;
+    // })
+    // .addCase(deleteContact.rejected, (state, { payload }) => {
+    //   state.error = payload;
+    //   state.loading = false;
+    // });
   },
 });
 
